1f12d759c2647206cdfab99291d6c23a2ff89b88
ApexUmrao <umraoshivanshu@gmail.com>
2026-05-28 23:41:15 +0530

    API Gatewway Server Added

---
+++ ApexBank/apigatewayserver/src/test/java/com/apex/apigatewayserver/ApigatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apex.apigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
