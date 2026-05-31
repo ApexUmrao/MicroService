cc5153e46c8a6fedbcdc57f7b4929307f3200fd1
ApexUmrao <umraoshivanshu@gmail.com>
2026-05-31 11:02:22 +0530

    Custom Routing Enables

--- ApexBank/apigatewayserver/src/main/java/com/apex/apigatewayserver/ApigatewayserverApplication.java
+++ ApexBank/apigatewayserver/src/main/java/com/apex/apigatewayserver/ApigatewayserverApplication.java
@@ -2,14 +2,37 @@ package com.apex.apigatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ApigatewayserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApigatewayserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatewayserverApplication.class, args);
+    }
 
+    public RouteLocator  getRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/apexbank/accounts/**")
+                        .filters( f -> f.rewritePath("/apexbank/accounts/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://ACCOUNTS"))
+                .route(p -> p
+                        .path("/apexbank/loans/**")
+                        .filters( f -> f.rewritePath("/apexbank/loans/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://LOANS"))
+                .route(p -> p
+                        .path("/apexbank/cards/**")
+                        .filters( f -> f.rewritePath("/apexbank/cards/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://CARDS")).build();
+
+
+
+    }
 }
